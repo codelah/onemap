7b43d2124dca42acde854460892868604f188e93
Ruiwen Chua <ruiwen@thoughtmonkeys.com>
2016-06-23 20:55:58 +0800

    Update setup data

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from distutils.core import setup
+from setuptools import setup
 
 try:
-  import pypandoc
-  readme = pypandoc.convert("README.md", "rst")
+  readme = open("README.rst").read()
 except (IOError, ImportError):
   readme = open("README.md").read()
 
 setup(
     name='onemap',
-    version='0.2.2',
+    version='0.2.2.post7',
     author='Ruiwen Chua',
-    author_email='ruiwen@99.co',
+    author_email='ruiwen@thoughtmonkeys.com',
     packages=['onemap'],
-    url='https://github.com/99co/onemap',
+    url='https://github.com/ruiwen/onemap',
     license='LICENSE.txt',
     description='Wrapper for the OneMapSG API',
     long_description=readme,
